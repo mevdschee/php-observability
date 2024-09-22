2b63b7b96684948e48965cea5a88907c88906811
Maurits van der Schee <maurits@vdschee.nl>
2024-09-22 22:28:06 +0200

    first commit

--- statistics/statistics.go
+++ statistics/statistics.go
@@ -81,7 +81,7 @@ func (s *Statistics) Write(writer *http.ResponseWriter) {
 		tagName := parts[1]
 		// counters
 		(*writer).Write([]byte("# HELP " + metricName + "_seconds A counter of the " + strings.ReplaceAll(metricName, "_", " ") + ".\n"))
-		(*writer).Write([]byte("# TYPE " + metricName + "_seconds counter\n"))
+		(*writer).Write([]byte("# TYPE " + metricName + "_seconds summary\n"))
 		var keys []string
 		for key := range ss.counters {
 			keys = append(keys, key)
