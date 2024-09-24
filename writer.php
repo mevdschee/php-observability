50927a08386d9580d1926edb40932d3d1808e119
Maurits van der Schee <maurits@vdschee.nl>
2024-09-24 13:27:46 +0200

    first commit

--- writer.php
+++ writer.php
@@ -1,9 +1,7 @@
 <?php
 
 while (true) {
-  if (Observer::logging()) {
-    Observer::log("database_calls", "file", "file.php@L123", 1 / 7);
-  }
+  Observer::log("database_calls", "file", "file.php@L123", 1 / 7);
   usleep(100 * 1000);
 }
 
@@ -16,7 +14,7 @@ class Observer
   private static bool $connected = false;
   private static int $connectAt = 0;
 
-  public static function logging(bool $connect = true): bool
+  public static function log(string $line)
   {
     if (!self::$socket) {
       self::$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) ?: null;
@@ -25,18 +23,12 @@ class Observer
     }
     if (!self::$connected) {
       $now = time();
-      if ($connect && self::$connectAt != $now) {
+      if (self::$connectAt != $now) {
         self::$connectAt = $now;
         self::$connected = @socket_connect(self::$socket, self::$address, self::$port);
       }
     }
-    return self::$connected;
-  }
-
-  public static function log(string $name, string $tagName, string $tag, float $duration)
-  {
     if (self::$connected) {
-      $line = sprintf("%s:%s:%s:%g", $name, $tagName, $tag, $duration);
       if (!@socket_write(self::$socket, $line . "\n", strlen($line) + 1)) {
         self::$socket = null;
         self::$connected = false;
