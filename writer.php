a6c169c84bbf7a7201da623192270cbb0789eb63
Maurits van der Schee <maurits@vdschee.nl>
2024-09-25 00:46:29 +0200

    add logging fn

--- writer.php
+++ writer.php
@@ -1,7 +1,7 @@
 <?php
 
 while (true) {
-  MetricObserver::log("database_calls", "file", "file.php@L123", 0.142857);
+  MetricObserver::log("database_calls", "file", "src/Controller/UserController.php@L123", 0.142857);
   usleep(100 * 1000);
 }
 
