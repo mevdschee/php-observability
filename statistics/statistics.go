9ee2dfcd4de26ea5f3b58dd54d10d8b4f67ecfe6
Maurits van der Schee <maurits@vdschee.nl>
2024-09-25 21:50:27 +0200

    update

--- statistics/statistics.go
+++ statistics/statistics.go
@@ -108,5 +108,5 @@ func (s *Statistics) Write(writer *http.ResponseWriter) {
 		(*writer).Write([]byte(metricName + "_total_seconds_sum " + strconv.FormatFloat(sum, 'f', 3, 64) + "\n"))
 		(*writer).Write([]byte(metricName + "_total_seconds_count " + strconv.FormatUint(count, 10) + "\n"))
 	}
-	(*writer).Write([]byte("# EOF"))
+	(*writer).Write([]byte("# EOF\n"))
 }
