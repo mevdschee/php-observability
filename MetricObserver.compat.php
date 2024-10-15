6548989a99b2f27914075371f46d43c13ff66e48
Maurits van der Schee <maurits@vdschee.nl>
2024-10-15 22:44:56 +0200

    update

--- MetricObserver.compat.php
+++ MetricObserver.compat.php
@@ -8,10 +8,13 @@ class MetricObserver
   private static $connected = false;
   private static $connectAt = 0;
 
-  public static function isConnected(): bool
+  public static function isConnected()
   {
     if (self::$socket === null) {
-      self::$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) ?: null;
+      self::$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+      if (!self::$socket) {
+        self::$socket = null;
+      }
       self::$connected = false;
     }
     if (!self::$connected) {
@@ -24,13 +27,13 @@ class MetricObserver
     return self::$connected;
   }
 
-  public static function log(string $metricName, string $labelName, string $labelValue, float $duration = null)
+  public static function log($metricName, $labelName, $labelValue, $duration = null)
   {
     if (self::isConnected()) {
       if ($duration === null) {
-        $line = json_encode([$metricName, $labelName, $labelValue]);
+        $line = json_encode(array($metricName, $labelName, $labelValue));
       } else {
-        $line = json_encode([$metricName, $labelName, $labelValue, (string)$duration]);
+        $line = json_encode(array($metricName, $labelName, $labelValue, (string)$duration));
       }
       if (!@socket_write(self::$socket, $line . "\n", strlen($line) + 1)) {
         self::$socket = null;
