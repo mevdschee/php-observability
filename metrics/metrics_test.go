8868e579f4d3aba499b4da39cfbf22caea86bf8a
Maurits van der Schee <maurits@vdschee.nl>
2024-10-30 02:22:46 +0100

    add test

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 )
 
+// TestAdd adds a value and checks that the value is stored.
 func TestAdd(t *testing.T) {
 	stats := New()
 	stats.Add("name", "label", "value", 1.23)
@@ -29,6 +30,7 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+// TestInc increments a value and checks that the value is stored.
 func TestInc(t *testing.T) {
 	stats := New()
 	stats.Inc("name", "label", "value", 1)
@@ -45,6 +47,31 @@ func TestInc(t *testing.T) {
 	}
 	got := string(body)
 	want := "name_total{label=\"value\"} 1"
+	if !strings.Contains(got, want) {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
+// TestAddMetrics adds two metrics and checks that the sum is stored.
+func TestAddMetrics(t *testing.T) {
+	stats := New()
+	stats.Add("name", "label", "value", 1.23)
+	stats2 := New()
+	stats2.Add("name", "label", "value", 1.23)
+	stats.AddMetrics(stats2)
+	w := httptest.NewRecorder()
+	stats.Write(w)
+	resp := w.Result()
+	gz, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		t.Errorf("error reading gz: %q", err.Error())
+	}
+	body, err := io.ReadAll(gz)
+	if err != nil {
+		t.Errorf("error reading body: %q", err.Error())
+	}
+	got := string(body)
+	want := "name_seconds_count{label=\"value\"} 2\nname_seconds_sum{label=\"value\"} 2.460"
 	if !strings.Contains(got, want) {
 		t.Errorf("got %s, want %s", got, want)
 	}
