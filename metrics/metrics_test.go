75628e659628e2736aca0ae6224f505efe3311ea
Maurits van der Schee <maurits@vdschee.nl>
2024-10-30 02:16:40 +0100

    add test

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -28,3 +28,24 @@ func TestAdd(t *testing.T) {
 		t.Errorf("got %s, want %s", got, want)
 	}
 }
+
+func TestInc(t *testing.T) {
+	stats := New()
+	stats.Inc("name", "label", "value", 1)
+	w := httptest.NewRecorder()
+	stats.Write(w)
+	resp := w.Result()
+	gz, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		t.Errorf("error reading gz: %q", err.Error())
+	}
+	body, err := io.ReadAll(gz)
+	if err != nil {
+		t.Errorf("error reading body: %q", err.Error())
+	}
+	got := string(body)
+	want := "name_total{label=\"value\"} 1"
+	if !strings.Contains(got, want) {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
